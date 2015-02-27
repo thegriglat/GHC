bb529f9363d48f05a530d45d9baa650cb711722f
Latyshev Grigory <grigorii.latyshev@cern.ch>
2015-02-27 19:43:34 +0100

    Fix output

--- pedestals.py
+++ pedestals.py
@@ -26,12 +26,13 @@ print "{classn:40s} | {empty:5s} | {tags:12s} | {empty:5s} | {tags:12s} | {empty
 
 print "-----------------------------------------------------------------------------------------------------------"
 shorter = DataP.getChannelsByFlag
-print "{0:40s} | {1:5d} | {2:12s} | {3:5d} | {4:12s} | {5:5d} | {6:12s}".format("Dead pedestal channels", len(shorter("DPG1")), "DPG1", len(shorter("BPG6")), "BPG6", len(shorter("DPG12")), "DPG12")
+print "{0:40s} | {1:5d} | {2:12s} | {3:5d} | {4:12s} | {5:5d} | {6:12s}".format("Dead pedestal channels", len(shorter("DPG1")), "DPG1", len(shorter("DPG6")), "DPG6", len(shorter("DPG12")), "DPG12")
 print "{0:40s} | {1:5d} | {2:12s} | {3:5d} | {4:12s} | {5:5d} | {6:12s}".format("Pedestal mean outside [170,230]", len(shorter("BPG1")), "BPG1", len(shorter("BPG6")), "BPG6", len(shorter("BPG12")), "BPG12")
 print "{0:40s} | {1:5d} | {2:12s} | {3:5d} | {4:12s} | {5:5d} | {6:12s}".format("Large RMS (moisy channels)", len(shorter("LRG1")), "LRG1", len(shorter("LRG6")), "LRG6", len(shorter("LRG12")), "LRG12")
 print "{0:40s} | {1:5d} | {2:12s} | {3:5d} | {4:12s} | {5:5d} | {6:12s}".format("Very large RMS (very noisy channels)", len(shorter("VLRG1")), "VLRG1", len(shorter("VLRG6")), "VLRG6", len(shorter("VLRG12")), "VLRG12")
 print "{0:40s} | {1:5d} | {2:12s} | {3:5d} | {4:12s} | {5:5d} | {6:12s}".format("Bad pedestal and noisy channels", len(shorter(["BPG1", "LRG1"])), "BPG1+LRG1", len(shorter(["BPG6","LRG6"])), "BPG6+LRG6", len(shorter(["BPG12","LGR12"])), "BPG12+LRG12")
 print "{0:40s} | {1:5d} | {2:12s} | {3:5d} | {4:12s} | {5:5d} | {6:12s}".format("Bad pedestal and very noisy", len(shorter(["BPG1","VLRG1"])), "BPG1+VLRG1", len(shorter(["BPG6","VLGR6"])), "BPG6+VLRG6", len(shorter(["BPG12","VLRG12"])), "BPG12+VLRG12")
+print "-----------------------------------------------------------------------------------------------------------"
 
 del shorter
 
