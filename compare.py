cb6746c5dd498ab37a8557e4b5d5a53c03f34a19
Latyshev Grigory <grigorii.latyshev@cern.ch>
2015-05-13 15:09:00 +0200

    added -jo + -s options in compare.py

--- compare.py
+++ compare.py
@@ -9,6 +9,8 @@ import log
 
 parser = argparse.ArgumentParser()
 parser.add_argument('files', metavar="JSONfile", nargs="+", help = "File(s) to analyse.")
+parser.add_argument('-jo', '--join', help="Join multiple JSON files into one", dest='joinjon')
+parser.add_argument('-s', '--summary', help="Print channels summary", action="store_true", dest="summary")
 args = parser.parse_args()
 
 def transformdata(data):
@@ -44,17 +46,25 @@ log.info("Trasform data structure ...")
 data = transformdata(data)
 log.info("Finished.")
 
-pregexp = "^[BD]P|^[B]LR"
-tpregexp = "^[DSL]TP"
-lregexp = "^[DS]LAMPL|LLERRO"
+if args.joinjson:
+  log.info("Writing merged data to file + " + args.joinjson + ' ...')
+  import json
+  json.dump(data, open(args.joinjson, 'w'))
+  log.info("Finished.")
 
-print "=== Summary Total Problematic Channels ==="
-print "  Total problematic channels                  :", sum([1 for c in data.keys() if len(data[c]['flags']) != 0])
-print "  Pedestals problems only                     :", sum([1 for c in data.keys() if has_regexp(pregexp, data[c]['flags'])])
-print "  Test Pulse problems only                    :", sum([1 for c in data.keys() if has_regexp(tpregexp, data[c]['flags'])])
-print "  Laser problems only                         :", sum([1 for c in data.keys() if has_regexp(lregexp, data[c]['flags'])])
-print "  Pedestals + Test Pulse problems only        :", sum([1 for c in data.keys() if has_regexp([pregexp,tpregexp], data[c]['flags'])])
-print "  Pedestals + Laser problems only             :", sum([1 for c in data.keys() if has_regexp([pregexp,lregexp], data[c]['flags'])])
-print "  Test Pulse + Laser problems only            :", sum([1 for c in data.keys() if has_regexp([tpregexp,lregexp], data[c]['flags'])])
-print "  Pedestals + Test Pulse + Laser problems only:", sum([1 for c in data.keys() if has_regexp([pregexp,tpregexp], data[c]['flags'])])
+
+if args.summary:
+  pregexp = "^[BD]P|^[B]LR"
+  tpregexp = "^[DSL]TP"
+  lregexp = "^[DS]LAMPL|LLERRO"
+
+  print "=== Summary Total Problematic Channels ==="
+  print "  Total problematic channels                  :", sum([1 for c in data.keys() if len(data[c]['flags']) != 0])
+  print "  Pedestals problems only                     :", sum([1 for c in data.keys() if has_regexp(pregexp, data[c]['flags'])])
+  print "  Test Pulse problems only                    :", sum([1 for c in data.keys() if has_regexp(tpregexp, data[c]['flags'])])
+  print "  Laser problems only                         :", sum([1 for c in data.keys() if has_regexp(lregexp, data[c]['flags'])])
+  print "  Pedestals + Test Pulse problems only        :", sum([1 for c in data.keys() if has_regexp([pregexp,tpregexp], data[c]['flags'])])
+  print "  Pedestals + Laser problems only             :", sum([1 for c in data.keys() if has_regexp([pregexp,lregexp], data[c]['flags'])])
+  print "  Test Pulse + Laser problems only            :", sum([1 for c in data.keys() if has_regexp([tpregexp,lregexp], data[c]['flags'])])
+  print "  Pedestals + Test Pulse + Laser problems only:", sum([1 for c in data.keys() if has_regexp([pregexp,tpregexp], data[c]['flags'])])
 
