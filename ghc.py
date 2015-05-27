55c8c5517520875725d3ee5443fd51113fc2b55b
Latyshev Grigory <grigorii.latyshev@cern.ch>
2015-05-27 10:20:17 +0200

    finished stage 1

--- ghc.py
+++ ghc.py
@@ -11,12 +11,14 @@ import Data
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--output', help="Results directory (default: RESULTS)", dest='output')
+parser.add_argument('-d', '--dbdump', help="Dump internal database", dest='dbdump')
 parser.add_argument('-c', '--dbstr', help="Connection string to DB (oracle://user/pass@db)", dest='dbstr')
 parser.add_argument('-pon', help="Pedestal HV ON runs", dest='pon_runs')
 parser.add_argument('-poff',help="Pedestal HV OFF runs)", dest='poff_runs')
 parser.add_argument('-tp',  help="Test Pulse runs", dest='tp_runs')
 parser.add_argument('-l',help="Laser runs", dest='l_runs')
-parser.add_argument('-lt', '--lasertable', help="Laser table", dest='lasertable')
+parser.add_argument('-lt', '--lasertable', help="Laser table", dest='lasertable', default = "MON_LASER_BLUE_DAT")
+
 args = parser.parse_args()
 
 if not args.output:
@@ -26,7 +28,7 @@ else:
 if not os.path.exists(outputdir):
   os.mkdir(outputdir)
 
-if args.dbstr is None:
+if not args.dbstr:
   print "Please specify --dbstr!"
   sys.exit(0)
 source = args.dbstr
@@ -49,55 +51,138 @@ if args.l_runs:
 
 GHC.classifyChannels()
 
+if args.dbdump != None:
+  GHC.Export(args.dbdump)
+
+print " ========================="
 print " === PEDESTAL ANALYSIS ==="
-print "=== {0} ANALYSIS ===".format(("EE", "EB")[D == DataEB])
-act = len ([ c for c in GHC.getActiveChannels() if  Data.getChannelClass(c) == "EB"])
-print "Number of inactive channels : {0}".format(61200 - act)
-print "Number of active channels   : {0}".format(act)
+print " ========================="
 
-print "Statistics of channels by problem classes: "
-print "{classn:45s} | {empty:5s} | {tags:12s}".format(classn = "Classes of pedestal problematic channels", empty="", tags="Short name")
+for d in ["EB", "EE"]:
+  print " === PEDESTAL {0} ANALYSIS ===".format(d)
 
-print "-----------------------------------------------------------------------------------------------------------"
-shorter = GHC.getChannelsByFlag
-for k in ["G1", "G6", "G12"]:
-  print "{0:45s} | {1:5d} | {2:12s}".format("Dead pedestal channels", len(shorter("DP" + k)), "DP" + k)
-  print "{0:45s} | {1:5d} | {2:12s}".format("Pedestal mean outside [170,230]", len(shorter("BP" + k)), "BP" + k)
-  print "{0:45s} | {1:5d} | {2:12s}".format("Large RMS (noisy channels)", len(shorter("LR" + k)), "LR" + k)
-  print "{0:45s} | {1:5d} | {2:12s}".format("Very large RMS (very noisy channels)", len(shorter("VLR" + k)), "VLR" + k)
-  print "{0:45s} | {1:5d} | {2:12s}".format("Bad pedestal and noisy channels", len(shorter(["BP" + k, "LR" + k])), "BP" + k +"+LR" + k)
-  print "{0:45s} | {1:5d} | {2:12s}".format("Bad pedestal and very noisy", len(shorter(["BP" + k,"VLR" + k])), "BP" + k + "+VLR" + k)
+  act = len ([ c for c in GHC.getActiveChannels(type=['pedestal_hvon', 'pedestal_hvoff']) if  Data.getChannelClass(c) == d])
+  print "Number of inactive channels : {0}".format((61200,14648)[d == "EE"] - act)
+  print "Number of active channels   : {0}".format(act)
+
+  print "Statistics of channels by problem classes: "
+  print "{classn:45s} | {empty:5s} | {tags:12s}".format(classn = "Classes of pedestal problematic channels", empty="", tags="Short name")
+
   print "-----------------------------------------------------------------------------------------------------------"
-del shorter
+  shorter = lambda x: len([ c for c in GHC.getChannelsByFlag(x) if Data.getChannelClass(c) == d]) 
+  for k in ["G1", "G6", "G12"]:
+    print "{0:45s} | {1:5d} | {2:12s}".format("Dead pedestal channels", shorter("DP" + k), "DP" + k)
+    print "{0:45s} | {1:5d} | {2:12s}".format("Pedestal mean outside [170,230]", shorter("BP" + k), "BP" + k)
+    print "{0:45s} | {1:5d} | {2:12s}".format("Large RMS (noisy channels)", shorter("LR" + k), "LR" + k)
+    print "{0:45s} | {1:5d} | {2:12s}".format("Very large RMS (very noisy channels)", shorter("VLR" + k), "VLR" + k)
+    print "{0:45s} | {1:5d} | {2:12s}".format("Bad pedestal and noisy channels", shorter(["BP" + k, "LR" + k]), "BP" + k +"+LR" + k)
+    print "{0:45s} | {1:5d} | {2:12s}".format("Bad pedestal and very noisy", shorter(["BP" + k,"VLR" + k]), "BP" + k + "+VLR" + k)
+    print "-----------------------------------------------------------------------------------------------------------"
+  del shorter
 
-print ""
+  print ""
 
-tpc = 0
-print "Get statistics by FLAGS:"
-for k in ["G1", "G6", "G12"]:
-  for i in GHC.PEDESTAL_FLAGS:
-    num = GHC.cur.execute("select count( distinct channel_id) from flags where flag = '{0}'".format(i + k)).fetchone()[0]
-    print "  {0:8s} : {1:5d}".format(i + k, num)
-print "Total problematic pedestal channels:", tpc
+  tpc = 0
+  print "Get statistics by FLAGS:"
+  for k in ["G1", "G6", "G12"]:
+    for i in GHC.PEDESTAL_FLAGS:
+      num = GHC.cur.execute("select count( distinct channel_id) from flags where flag = '{0}' and channel_id like '{1}%'".format(i + k, (2,1)[d == "EB"])).fetchone()[0]
+      tpc += num
+      print "  {0:8s} : {1:5d}".format(i + k, num)
+  print "Total problematic pedestal channels:", tpc
 
-i = 1/0
-print ""
-if not os.path.exists(outputdir + "/pedestals"):
-  os.mkdir(outputdir + "/pedestals")
-for i in activekeys:
-  for j in (True, False):
-    h = D.get1DHistogram(i, None,  j)
-    Data.saveHistogram(h, outputdir + "/pedestals/{0}{1}_EB.1D.pdf".format(i, ("", "_RMS")[j])) 
-    del h
-    plttype = ("barrel", "endcap")[D == DataEE]
-    h = D.get2DHistogram(i, j, plottype = plttype)
-    Data.saveHistogram(h, outputdir + "/pedestals/{0}{1}_{2}.2D.pdf".format(i, ("", "_RMS")[j],("EE","EB")[D == DataEB]), plttype) 
-    del h
-print "    === END PEDESTALS {0} ===".format(("EE", "EB")[D == DataEB])
+  print ""
 
-if args.json:
-  log.info ("Save json file " + args.json + ' ...')
-  sumdict = {}
-  sumdict.update(DataEB.getChannels())
-  sumdict.update(DataEE.getChannels())
-  Data.jsonExport(sumdict, open(args.json, 'w'))
+
+print " =========================="
+print " === TESTPULSE ANALYSIS ==="
+print " =========================="
+
+for d in ("EB", "EE"):
+  print "=== TEST PULSE {0} ANALYSIS ===".format(d)
+  act = len ([ c for c in GHC.getActiveChannels(type = 'testpulse') if  Data.getChannelClass(c) == d])
+  print "Number of inactive channels : {0}".format((61200,14648)[d == "EE"] - act)
+  print "Number of active channels   : {0}".format(act)
+
+  print "Statistics of channels by problem classes: "
+  print "{classn:45s} | {empty:5s} | {tags:12s}".format(classn = "Classes of Test Pulse problematic channels", empty="", tags="Short name")
+
+  print "-----------------------------------------------------------------------------------------------------------"
+  shorter = lambda x: len([ c for c in GHC.getChannelsByFlag(x) if Data.getChannelClass(c) == d]) 
+  for k in ("G1", "G2", "G12"):
+    print "{0:45s} | {1:5d} | {2:12s}".format("Dead TP channels", shorter("DTP" + k), "DTP" + k)
+    print "{0:45s} | {1:5d} | {2:12s}".format("Low TP amplitude", shorter("STP" + k), "STP" + k)
+    print "{0:45s} | {1:5d} | {2:12s}".format("Large TP amplitude", shorter("LTP" + k), "LTP" + k)
+    print "-----------------------------------------------------------------------------------------------------------"
+  del shorter
+
+  tpc = 0
+  print "Get statistics by FLAGS:"
+  for k in ("G1", "G6", "G12"):
+    for i in GHC.TESTPULSE_FLAGS:
+      num = GHC.cur.execute("select count( distinct channel_id) from flags where flag = '{0}' and channel_id like '{1}%'".format(i + k, (2,1)[d == "EB"])).fetchone()[0]
+      tpc += num
+      print "  {0:8s} : {1:5d}".format(i + k, num)
+  print "Total problematic test pulse channels:", tpc
+
+
+
+print " ======================"
+print " === LASER ANALYSIS ==="
+print " ======================"
+
+for D in ("EB", "EE"):
+  print "=== LASER {0} ANALYSIS ===".format(d)
+  act = len ([ c for c in GHC.getActiveChannels(type = 'laser') if  Data.getChannelClass(c) == d])
+  print "Number of inactive channels : {0}".format((61200,14648)[d == "EE"] - act)
+  print "Number of active channels   : {0}".format(act)
+
+  print "Getting info per error key :"
+  shorter = lambda x: len([ c for c in GHC.getChannelsByFlag(x) if Data.getChannelClass(c) == d]) 
+  for i in GHC.LASER_FLAGS:
+    print "{0:15s} : {1:5d}".format(i, shorter(i))
+
+
+# plotting
+for i in ['pedestals_hvon', 'pedestals_hvoff', 'testpulse', 'laser']:
+  if not os.path.exists(outputdir + "/" + i):
+      os.mkdir(outputdir + "/" + i)
+
+for d in ("EB", "EE"):
+  for rms in (True, False):
+    for i in ("G1", "G6", "G12"):
+      ### 1D plots
+      h = GHC.get1DHistogram(key = "PED_{0}_".format(('MEAN', 'RMS')[rms]) + i, useRMS = rms, type="pedestal_hvon", part = d, name = "Pedestal {0}, gain {1} ({2})".format(('mean', 'RMS')[rms], i, "HV ON"))
+      Data.saveHistogram(h, outputdir + "/pedestals_hvon/{0}_{1}_{2}.1D.pdf".format(i, ("MEAN", "RMS")[rms], d), d)
+
+      h = GHC.get1DHistogram(key = "PED_{0}_".format(('MEAN', 'RMS')[rms]) + i, useRMS = rms, type="pedestal_hvoff", part = d ,name = "Pedestal {0}, gain {1} ({2})".format(('mean', 'RMS')[rms], i, "pedestal_hvoff"))
+      Data.saveHistogram(h, outputdir + "/pedestals_hvoff/{0}_{1}_{2}.1D.pdf".format(i, ("MEAN", "RMS")[rms], d), d)
+    
+      h = GHC.get1DHistogram(key = "ADC_{0}_".format(('MEAN', 'RMS')[rms]) + i, useRMS = rms, type="testpulse", part = d, name = "Test Pulse {0}, gain {1}".format(('mean', 'RMS')[rms], i))
+      Data.saveHistogram(h, outputdir + "/testpulse/{0}_{1}_{2}.1D.pdf".format(i, ("MEAN", "RMS")[rms], d), d)
+      
+      ### 2D plots
+      h = GHC.get2DHistogram(key = "PED_{0}_".format(('MEAN', 'RMS')[rms]) + i, useRMS = rms, type="pedestal_hvon", part = d, name = "Pedestal {0}, gain {1} ({2})".format(('mean', 'RMS')[rms], i, "HV ON"))
+      Data.saveHistogram(h, outputdir + "/pedestals_hvon/{0}_{1}_{2}.2D.pdf".format(i, ("MEAN", "RMS")[rms], d), d)
+      h = GHC.get2DHistogram(key = "PED_{0}_".format(('MEAN', 'RMS')[rms]) + i, useRMS = rms, type="pedestal_hvoff", part = d, name = "Pedestal {0}, gain {1} ({2})".format(('mean', 'RMS')[rms], i, "pedestal_hvoff"))
+      Data.saveHistogram(h, outputdir + "/pedestals_hvoff/{0}_{1}_{2}.2D.pdf".format(i, ("MEAN", "RMS")[rms], d), d)
+      h = GHC.get2DHistogram(key = "ADC_{0}_".format(('MEAN', 'RMS')[rms]) + i, useRMS = rms, type="testpulse", part = d, name = "Test Pulse {0}, gain {1}".format(('mean', 'RMS')[rms], i))
+      Data.saveHistogram(h, outputdir + "/testpulse/{0}_{1}_{2}.2D.pdf".format(i, ("MEAN", "RMS")[rms], d), d)
+
+  ### laser plots
+    h = GHC.get1DHistogram(key = "APD_{0}".format(('MEAN', 'RMS')[rms]), useRMS = rms, type="laser", part = d, name = "Laser {0}, ({1})".format(('mean', 'RMS')[rms] , args.lasertable))
+    Data.saveHistogram(h, outputdir + "/laser/Laser_{0}_{1}.1D.pdf".format(("MEAN", "RMS")[rms], d), d)
+    h = GHC.get1DHistogram(key = "APD_OVER_PN_{0}".format(('MEAN', 'RMS')[rms]), useRMS = rms, type="laser", part = d, name = "APD/PN {0}, ({1})".format(('mean', 'RMS')[rms], args.lasertable))
+    Data.saveHistogram(h, outputdir + "/laser/APDPN_{0}_{1}.1D.pdf".format(("MEAN", "RMS")[rms], d), d)
+    h = GHC.get2DHistogram(key = "APD_{0}".format(('MEAN', 'RMS')[rms]), useRMS = rms, type="laser", part = d, name = "Laser {0}, ({1})".format(('mean', 'RMS')[rms] , args.lasertable))
+    Data.saveHistogram(h, outputdir + "/laser/Laser_{0}_{1}.2D.pdf".format(("MEAN", "RMS")[rms], d), d)
+    h = GHC.get1DHistogram(key = "APD_OVER_PN_{0}".format(('MEAN', 'RMS')[rms]), useRMS = rms, type="laser", part = d, name = "APD/PN {0}, ({1})".format(('mean', 'RMS')[rms] , args.lasertable))
+    Data.saveHistogram(h, outputdir + "/laser/APDPN_{0}_{1}.2D.pdf".format(("MEAN", "RMS")[rms], d), d)
+
+      
+
+
+
+#h = GHC.get2DHistogram(type="pedestal_hvon", key = "PED_MEAN_G1", useRMS=False, part = "EE")
+#Data.saveHistogram(h, "{0}{1}_EB.2D.pdf".format("G1","MEAN"), plottype='endcap') 
+
