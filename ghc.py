3dd8dab6d54a33f2429db392c544bc54276ade14
Latyshev Grigory <grigorii.latyshev@cern.ch>
2015-05-27 15:10:01 +0200

    Improve printing

--- ghc.py
+++ ghc.py
@@ -33,11 +33,11 @@ if not args.dbstr:
   sys.exit(0)
 source = args.dbstr
 
+header = lambda x: "="*((78 - (len(x)/2*2))/2) + " " + x + " " + "="*((78 - len(x))/2)
+
 GHC = Data.Data()
 GHC.readAllChannels("data/EB_all_ch.txt")
 GHC.readAllChannels("data/EE_all_ch.txt")
-
-#GHC.readData(source, runs=runs, type="pedestal_hvoff")
 
 if args.pon_runs:
   GHC.readData(source, runs=args.pon_runs.split(), type="pedestal_hvon")
@@ -52,17 +52,19 @@ if args.l_runs:
 GHC.classifyChannels()
 
 if args.dump != None:
-  GHC.Export(args.dbdump)
+  GHC.Export(args.dump)
 if args.dumpsql != None:
-  GHC.DumpSQL(args.dbdump)
+  Data.DumpSQL(GHC.dbh, args.dumpsql)
 
-print " ========================="
-print " === PEDESTAL ANALYSIS ==="
-print " ========================="
+
+print "="*80
+print header("PEDESTAL ANALYSIS")
+print "="*80
+print ""
 
 for d in ["EB", "EE"]:
-  print " === PEDESTAL {0} ANALYSIS ===".format(d)
-
+  print header("PEDESTAL {0} ANALYSIS".format(d))
+  print ""
   act = len ([ c for c in GHC.getActiveChannels(type=['pedestal_hvon', 'pedestal_hvoff']) if  Data.getChannelClass(c) == d])
   print "Number of inactive channels : {0}".format((61200,14648)[d == "EE"] - act)
   print "Number of active channels   : {0}".format(act)
@@ -70,7 +72,7 @@ for d in ["EB", "EE"]:
   print "Statistics of channels by problem classes: "
   print "{classn:45s} | {empty:5s} | {tags:12s}".format(classn = "Classes of pedestal problematic channels", empty="", tags="Short name")
 
-  print "-----------------------------------------------------------------------------------------------------------"
+  print "-"*80
   shorter = lambda x: len([ c for c in GHC.getChannelsByFlag(x) if Data.getChannelClass(c) == d]) 
   for k in ["G1", "G6", "G12"]:
     print "{0:45s} | {1:5d} | {2:12s}".format("Dead pedestal channels", shorter("DP" + k), "DP" + k)
@@ -79,7 +81,7 @@ for d in ["EB", "EE"]:
     print "{0:45s} | {1:5d} | {2:12s}".format("Very large RMS (very noisy channels)", shorter("VLR" + k), "VLR" + k)
     print "{0:45s} | {1:5d} | {2:12s}".format("Bad pedestal and noisy channels", shorter(["BP" + k, "LR" + k]), "BP" + k +"+LR" + k)
     print "{0:45s} | {1:5d} | {2:12s}".format("Bad pedestal and very noisy", shorter(["BP" + k,"VLR" + k]), "BP" + k + "+VLR" + k)
-    print "-----------------------------------------------------------------------------------------------------------"
+    print "-"*80
   del shorter
 
   print ""
@@ -96,12 +98,14 @@ for d in ["EB", "EE"]:
   print ""
 
 
-print " =========================="
-print " === TESTPULSE ANALYSIS ==="
-print " =========================="
+print "="*80
+print header("TEST PULSE ANALYSIS")
+print "="*80
+print ""
 
 for d in ("EB", "EE"):
-  print "=== TEST PULSE {0} ANALYSIS ===".format(d)
+  print header("TEST PULSE {0} ANALYSIS".format(d))
+  print ""
   act = len ([ c for c in GHC.getActiveChannels(type = 'testpulse') if  Data.getChannelClass(c) == d])
   print "Number of inactive channels : {0}".format((61200,14648)[d == "EE"] - act)
   print "Number of active channels   : {0}".format(act)
@@ -109,13 +113,13 @@ for d in ("EB", "EE"):
   print "Statistics of channels by problem classes: "
   print "{classn:45s} | {empty:5s} | {tags:12s}".format(classn = "Classes of Test Pulse problematic channels", empty="", tags="Short name")
 
-  print "-----------------------------------------------------------------------------------------------------------"
+  print "-"*80
   shorter = lambda x: len([ c for c in GHC.getChannelsByFlag(x) if Data.getChannelClass(c) == d]) 
   for k in ("G1", "G2", "G12"):
     print "{0:45s} | {1:5d} | {2:12s}".format("Dead TP channels", shorter("DTP" + k), "DTP" + k)
     print "{0:45s} | {1:5d} | {2:12s}".format("Low TP amplitude", shorter("STP" + k), "STP" + k)
     print "{0:45s} | {1:5d} | {2:12s}".format("Large TP amplitude", shorter("LTP" + k), "LTP" + k)
-    print "-----------------------------------------------------------------------------------------------------------"
+    print "-"*80
   del shorter
 
   tpc = 0
@@ -129,12 +133,16 @@ for d in ("EB", "EE"):
 
 
 
-print " ======================"
-print " === LASER ANALYSIS ==="
-print " ======================"
+
+print "="*80 
+print header("LASER ANALYSIS")
+print "="*80
+print ""
 
 for D in ("EB", "EE"):
-  print "=== LASER {0} ANALYSIS ===".format(d)
+  print header("LASER {0} ANALYSIS".format(d))
+  print ""
+
   act = len ([ c for c in GHC.getActiveChannels(type = 'laser') if  Data.getChannelClass(c) == d])
   print "Number of inactive channels : {0}".format((61200,14648)[d == "EE"] - act)
   print "Number of active channels   : {0}".format(act)
@@ -146,6 +154,7 @@ for D in ("EB", "EE"):
 
 
 # plotting
+print header("Preparing plots ...")
 for i in ['pedestals_hvon', 'pedestals_hvoff', 'testpulse', 'laser']:
   if not os.path.exists(outputdir + "/" + i):
       os.mkdir(outputdir + "/" + i)
