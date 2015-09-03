798bbe7cdc669c840dc1b9b8aea37e0ffcd5ec41
Grigorii Latyshev <grigorii.latyshev@cern.ch>
2015-09-03 07:43:24 +0200

    Added CVMFS path to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-export TNS_ADMIN=$(cd $(dirname ${BASH_SOURCE}); pwd -P)
+mount | grep -q '/cvmfs/cms.cern.ch' && export TNS_ADMIN=/cvmfs/cms.cern.ch/slc6_amd64_gcc491/cms/oracle-env/29/etc || export TNS_ADMIN=$(cd $(dirname ${BASH_SOURCE}); pwd -P)
